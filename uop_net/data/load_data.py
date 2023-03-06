e0b5f504533782e2adf57dfca8630e2dece1e8e9
dinnno <sjnoh7@naver.com>
2023-03-06 17:50:47 +0900

    add: load_data

---
+++ uop_net/data/load_data.py
@@ -0,0 +1,25 @@
+import torch
+
+from data.uop_sim import UOPSim, MergeDataset
+from torch.utils.data.dataset import random_split
+
+def load_data(opt):
+    """
+    root_1 : Custom ShapeNet root
+    root_2 : Custom 3DNet root
+    """
+    root_1 = opt.config['base']['env']['server']['lecun']['shapenet_root']
+    root_2 = opt.config['base']['env']['server']['lecun']['3dnet_root']
+
+    root_list = [root_1, root_2]
+    dataset_list = []
+    for root in root_list:
+        dataset_list.append(UOPSim(root,
+                            sampling='random',
+                            partial=False,  #TODO
+                            projection=False))
+    dataset = MergeDataset(dataset_list)
+    data_length  = dataset.dataset_size
+    tr_data, val_data = random_split(dataset, [round(data_length * 0.8), round(data_length * 0.2)], generator=torch.Generator().manual_seed(777))
+
+    return tr_data, val_data
