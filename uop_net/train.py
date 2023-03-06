6dada44506b161402a6e987cb0b1d61da95b2a4c
dinnno <sjnoh7@naver.com>
2023-03-06 22:55:49 +0900

    add: sample code for training

--- uop_net/train.py
+++ uop_net/train.py
@@ -28,3 +28,30 @@ from utility.pointcloud_utils import *
 from module.model.uop_net import UOPNet
 from module.losses.plane_loss import PlaneLoss
 from module.losses.stability_loss import StabilityLoss
+
+def train(opt, device):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', type=str, default='./config', 
+                        help='config file path')
+    
+    args = parser.parse_args()
+
+    opt = config(args.config_path)
+    opt.get_base_config()
+
+    '''setting base path such as ckp_path'''
+    save_dir = opt.config['base']['env']['save_dir']
+    exp_name = opt.config['base']['env']['exp_name']
+    exp_condition = opt.config['base']['env']['exp_condition']
+
+    ckp_dir = os.path.join(save_dir, exp_name, exp_condition)
+    os.makedirs(ckp_dir, exist_ok=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train(opt, device)
+
+
+
