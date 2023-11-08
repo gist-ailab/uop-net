76d33d822c43cfabe004450cf9aa370c7e76aef8
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-11-08 15:26:13 +0900

    feat: add pyrep to setup bash draft

--- setup_env_TODOs.sh
+++ setup_env_TODOs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 echo "-->> UOP-setup bash : start bash code"
 # TODO : make flag
 error_flag = 0
@@ -88,14 +91,25 @@ sh ./coppeliaSim.sh
 # sim install pyrep
 
 ## clone git
+#TODO : check is git installed ?
+cd ..
+git clone https://github.com/stepjam/PyRep.git
 
 ## add envrionment variable
+export COPPELIASIM_ROOT=EDIT/ME/PATH/TO/COPPELIASIM/INSTALL/DIR
+#TODO : change ROOT abs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
+export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 
 ## install pyrep
-
-##
+cd PyRep 
+#TODO : check, is env name uop env?
+conda activate uop # check duplicated
+pip install -r requirements.txt
+pip install .
 
 # test
+python -m import pyrep # is this right to check?
 
 
 
