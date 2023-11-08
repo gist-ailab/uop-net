ebda4fc37ae018985a5866a67058fe5b00d042bf
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-11-08 14:33:28 +0900

    feat: update setup bash with conda setup draft

--- setup_env_TODOs.sh
+++ setup_env_TODOs.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
+echo "-->> UOP-setup bash : start bash code"
+# TODO : make flag
+error_flag = 0
 
 
 
-val1 = 0
+# = check system
+#TODO : check gpu
+#TODO : check file storage
+#
 
 # check linux 20.04
+#TODO : check ubuntu
+echo "-->> UOP-setup bash : system check"
+
+mkdir uop-download
+cd uop-download
 # check nvidia driver ver
 # check cuda ver
 
 
-# = setup environment bash code
+
+
+# = setup conda environment
 
 # conda install
 ## download
+#TODO : check storage space
+#TODO : check linux ubuntu ver
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
 ## install
+#TODO : check file exist
+#TODO : check permission ? / chmod +777 ?
+sh ./Anaconda3-2023.09-0-Linux-x86_64.sh
+# sh ./Anaconda3-2023.09-0-Linux-x86_64.sh -b
+
 
 # conda create env 
 ## create env
+#TODO : check env name is exist?
+conda create -n uop
 ## activate
+conda activate uop
 
 # test
-
+#TODO : check current env name
 
 
 
