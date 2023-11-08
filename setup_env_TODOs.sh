c7dd7118fd6335e56d39ad6f94124f04c4d7b347
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-11-08 14:13:51 +0900

    feat: add setup code draft

---
+++ setup_env_TODOs.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+
+
+val1 = 0
+
+# check linux 20.04
+# check nvidia driver ver
+# check cuda ver
+
+
+# = setup environment bash code
+
+# conda install
+## download
+## install
+
+# conda create env 
+## create env
+## activate
+
+# test
+
+
+
+
+# sim install coppelia sim
+
+# turn on once.
+
+# add envrionment variable
+
+# sim install pyrep
+
+# test
+
+
+
+
+# conda install package
+
+
+
+
+# download dataset
+
+# check YCB dataset
+## read dataset
+## visualize dataset
+
+# check 3DNet dataset
+## read dataset
+## visualize dataset
+
+# check ShapeNet dataset
+## read dataset
+## visualize dataset
+
+# check UOP-Sim dataset
+## read dataset
+## visualize dataset
+
+
+
+
+# 
+
+
+
