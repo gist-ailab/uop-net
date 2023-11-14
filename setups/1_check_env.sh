b9d7d9a78654eaf74e8d83373bc24607acb9b006
ailabktw <ailab.ktw@gmail.com>
2023-11-14 14:58:15 +0900

    feat: update setup bash to check storage

--- setups/1_check_env.sh
+++ setups/1_check_env.sh
@@ -16,6 +16,11 @@ UOP_ERROR_FLAG=0
 
 
 # = check system
+
+# check linux 20.04
+#TODO : check ubuntu
+
+## check GPU
 UOP_GPU_STRING=$(lspci | grep asdf | cut -d" " -f 1)
 if [ -z $UOP_GPU_STRING ]
 then 
@@ -27,12 +32,17 @@ else
     echo "... [CHECK] GPU Detected. continue ... "
 fi
 
-#TODO : check file storage
-
-
-# check linux 20.04
-#TODO : check ubuntu
-
+## check Stroage
+UOP_STORAGE_STRING=$(df -PBG $PWD | tail -1 | awk '{print $4}' | tr "G" " ")
+if [ 2 -gt $UOP_STORAGE_STRING ];
+then 
+    echo "... [ERROR] Storage is Not Sufficient"
+    UOP_ERROR_FLAG=1
+    echo "-->> UOP-setup bash : End system check bash code with Error : $UOP_ERROR_FLAG"
+    exit $UOP_ERROR_FLAG
+else
+    echo "... [CHECK] Storage Checked. continue ... "
+fi
 
 # check nvidia driver ver
 # check cuda ver
