7bc9733ee99bd5b1a1d375ae60c412ca6a3f76c8
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-11-24 00:49:01 +0900

    feat: add uop_sim download draft

---
+++ models/download_uop_sim_dataset.py
@@ -0,0 +1,11 @@
+# pip install gdown
+import gdown
+
+gdown https://drive.google.com/uc?id=<file_id>
+# https://drive.google.com/file/d/11yvzrLgIbv8e3Yy2gyCG0k2QMHepeGa0/view?usp=drive_link
+
+# gdown <file_id>
+# gdown --folder https://drive.google.com/drive/folders/<file_id>
+# gdown --folder --id <file_id>
+
+
