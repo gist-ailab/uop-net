bd8ab0647c9b2083f1a4c2d32f67cc8a8019ab27
ailabktw <ailab.ktw@gmail.com>
2023-11-25 18:14:13 +0900

    feat: add file_selector code for select gif source

--- _temp/file_selector.py
+++ _temp/file_selector.py
@@ -13,47 +13,46 @@ import natsort
 
 if __name__=="__main__":
 
-    data_root = "F:\_device\_local\20231124_uop_ycb_rotate"
-    dst_root = "/home/ailab-ur5/_Workspaces/_data/uop/pub_dataset"
+    data_root = "/home/ailab-ur5/_Workspaces/_data/uop/20231124_uop_ycb_rotate/ycb/"
+    dst_root = "/home/ailab-ur5/_Workspaces/_data/uop/20231125_uop_ycb_rotate_selected/ycb/"
 
-    list_file_folders = glob.glob(data_root+'*[!.json]')
 
-    sorted_list_file_folders = natsort.natsorted(list_file_folders)
+    # list_obj_folder = os.listdir(data_root)
+    list_obj_folder = glob.glob(data_root + "*/")
+    sorted_list_obj_folder = natsort.natsorted(list_obj_folder)
+    
+    list_not_copied = []
+    for one_obj_folder in tqdm.tqdm(sorted_list_obj_folder):
+        list_img_folder = glob.glob(one_obj_folder + "recorded_data/*/")
+        sorted_list_img_folder = natsort.natsorted(list_img_folder)
 
-    list_not_copied = [] 
-    for one_dataset_folder in sorted_list_file_folders:
+        for one_img_folder in sorted_list_img_folder:
+            
+            list_img_file = glob.glob(one_img_folder + "*.png")
+            sorted_list_img_file = natsort.natsorted(list_img_file)
 
-        datas_folder = glob.glob(one_dataset_folder + '/*[!.png]')
-
-        print ()
-
-        sorted_datas_folder = natsort.natsorted(datas_folder)
-        for one_data_folder in tqdm.tqdm(sorted_datas_folder) :
-
-            datas = glob.glob(one_data_folder + '/[label]*')
-            # datas = glob.glob(one_data_folder + '/[label_inspected]*')    # : dosen't work
-            for one_data in datas:
-                # if "label_inspected" in one_data:
-                if "label_inspected" not in one_data:
+            for one_img in sorted_list_img_file:
+                img_num = one_img.split("/")[-1].replace(".png", "")
+                if 419 <= int(img_num):
                     continue
+
                 try:
-                    data_dir = one_data.split("/label")
-                    # data_dir = one_data.split("/label_inspected")
-                    saved_dir = data_dir[0].replace(
-                        "/media/ailab-ur5/T7/_storage/Server/NAS/ailab(PAT)/Dataset/SOP/dataset", 
-                        dst_root)
-                    os.makedirs(saved_dir, exist_ok=True)
-                    shutil.copyfile(one_data, saved_dir + f"/label{data_dir[-1]}")
+                    saved_dir = one_img.replace(data_root, dst_root)
+                    os.makedirs(saved_dir.replace(saved_dir.split("/")[-1], ""), exist_ok=True)
+                    shutil.copyfile(one_img, saved_dir)
                     # shutil.copyfile(one_data, saved_dir + f"/label_inspected{data_dir[-1]}")
                 except:
-                    print(f"!!>> {one_data} has not copied")
-                    list_not_copied.append(one_data)
-            # print ()
-        # print ()
-
+                    print(f"!!>> {one_img} has not copied")
+                    list_not_copied.append(list_not_copied)
+    
     print(list_not_copied)
 
-    print()
+
+
+            
+        
+
+
 
 
 
