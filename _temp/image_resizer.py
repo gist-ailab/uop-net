f3a2e4ab2562cafeb4170eb20d2afafeeffbc87d
ailabktw <ailab.ktw@gmail.com>
2023-11-26 17:54:40 +0900

    feat: update image_resizer

--- _temp/image_resizer.py
+++ _temp/image_resizer.py
@@ -8,6 +8,7 @@ import shutil
 
 # import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
 import tqdm
 import natsort
 
@@ -15,7 +16,8 @@ import natsort
 if __name__=="__main__":
 
     data_root = "/home/ailab-ur5/_Workspaces/_data/uop/20231125_uop_ycb_rotate_selected/ycb/"
-    dst_root = "/home/ailab-ur5/_Workspaces/_data/uop/20231126_uop_ycb_selected_resized/ycb/"
+    dst_root_1 = "/home/ailab-ur5/_Workspaces/_data/uop/20231126_uop_ycb_selected+cropped/ycb/"
+    dst_root_2 = "/home/ailab-ur5/_Workspaces/_data/uop/20231126_uop_ycb_selected+cropped+resized/ycb/"
 
 
     # list_obj_folder = os.listdir(data_root)
@@ -35,21 +37,48 @@ if __name__=="__main__":
 
             one_img_folder_split = one_img_folder.split("/")
 
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            plt.ion()
+            plt.show()
+
             for one_image_file in sorted_list_img_file:
                 try:
                     pil_image = Image.open(one_image_file).copy()
+
+                    # = crop
                     x_low = int((1920-1080) / 2)
                     y_low = 0
                     x_high = 1080 + int((1920-1080) / 2)
-                    y_high = 1079
+                    y_high = 1080
                     crop_box = (x_low, y_low, x_high, y_high)
                     crop_pil_image = pil_image.crop(crop_box).copy()
-                    crop_pil_image.show()
-                    # resized_pil_image = crop_pil_image.resize().copy()
-                    resized_pil_image.show()
+                    # # = show
+                    # plt_img = ax.imshow(crop_pil_image)   # : plt
+                    # plt.draw()
+                    # plt.imshow(crop_pil_image)
+                    # crop_pil_image.show()                 # : PIL
+                    save_name_1 = one_image_file.replace("20231125_uop_ycb_rotate_selected", "20231126_uop_ycb_selected+cropped")
+                    save_name_1_dir = save_name_1.replace(save_name_1.split("/")[-1], "")
+                    
+                    os.makedirs(save_name_1_dir, exist_ok=True)
+                    crop_pil_image.save(save_name_1)
 
-                    # gif_tag = one_img_folder_split[-2]
-                    # pil_images_first.save(one_obj_folder + f"{gif_tag}.gif", save_all=True, append_images=pil_images[1:], loop=0xff, duration=10)
+                    # = resize
+                    resize_wh = (512, 512)
+                    resized_pil_image = crop_pil_image.resize(resize_wh, resample=1).copy() # : nn resize
+                    # # = show
+                    # plt_img = ax.imshow(resized_pil_image)   # : plt
+                    # plt.draw()
+                    # plt.imshow(crop_pil_image)                # : or
+                    # resized_pil_image.show()                  # : PIL
+                    
+                    save_name_2 = one_image_file.replace("20231125_uop_ycb_rotate_selected", "20231126_uop_ycb_selected+cropped+resized")
+                    save_name_2_dir = save_name_2.replace(save_name_1.split("/")[-1], "")
+                    
+                    os.makedirs(save_name_2_dir, exist_ok=True)
+                    resized_pil_image.save(save_name_2)
+
                 except:
                     pass
                     obj_name = one_img_folder_split[-4]
