6729d5f706f81c49ea53206f2cd668a3433ef3a7
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-11-27 23:51:17 +0900

    feat: add UOP-Sim download code

--- models/download_uop_sim_dataset.py
+++ models/download_uop_sim_dataset.py
@@ -1,11 +1,14 @@
 # pip install gdown
 import gdown
 
-gdown https://drive.google.com/uc?id=<file_id>
-# https://drive.google.com/file/d/11yvzrLgIbv8e3Yy2gyCG0k2QMHepeGa0/view?usp=drive_link
+# = in python
+# file_id_1 = "11yvzrLgIbv8e3Yy2gyCG0k2QMHepeGa0"
+# gdown.download(id=file_id_1, quiet=False)
+# # output = "data/dataset.zip"
+# # gdown.download(id=file_id, output=output, quiet=False)
+# !! this file is too big for google drive (28G)
 
-# gdown <file_id>
-# gdown --folder https://drive.google.com/drive/folders/<file_id>
-# gdown --folder --id <file_id>
+file_id_2 = "19mmLYNT_2reMV7C7Z8pEWwgjBulVobCG"
+gdown.download(id=file_id_2, quiet=False)
 
-
+print()
---
+++ models/download_uop_sim_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# = in bash
+# gdown --id 11yvzrLgIbv8e3Yy2gyCG0k2QMHepeGa0
+# # https://drive.google.com/file/d/11yvzrLgIbv8e3Yy2gyCG0k2QMHepeGa0/view?usp=drive_link
+# !! this file is too big for google drive (28G)
+
+gdown --id 19mmLYNT_2reMV7C7Z8pEWwgjBulVobCG
+# https://drive.google.com/file/d/19mmLYNT_2reMV7C7Z8pEWwgjBulVobCG/view?usp=drive_link
+
+# # gdown <file_id>
+# # gdown --folder https://drive.google.com/drive/folders/<file_id>
+# # gdown --folder --id <file_id>
+
+echo ""
