c449b12883a6c0c5c7ad29330db0dd740d9bfbfb
dinnno <sjnoh7@naver.com>
2023-12-02 20:55:49 +0900

    add: final version for training uop-net

--- uop_net/data/load_data.py
+++ uop_net/data/load_data.py
@@ -1,7 +1,34 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 import torch
 from torch.utils.data.dataset import random_split
 
-from data.uop_sim import UOPSim, MergeDataset
+from dataset.uopsim import UOPSIM
+
+
+
+class MergeDataset(UOPSIM):
+    def __init__(self, dataset_list):
+        self.dataset_size = 0
+    
+        self.dataset_list = dataset_list
+    
+        self.size_list = []
+        for dataset in dataset_list:
+            self.dataset_size += dataset.dataset_size
+            self.size_list.append(self.dataset_size)
+    
+        print(">>>Merge Dataset Size: {}".format(self.dataset_size))
+    
+    def __getitem__(self, i):
+        prev = 0
+        for idx, size in enumerate(self.size_list):
+            if i < size:
+                return self.dataset_list[idx][i-prev]
+            prev = size
+
 
 
 def load_data(args):
@@ -12,13 +39,21 @@ def load_data(args):
     root_1 = args.shapenet_root
     root_2 = args.threednet_root
 
+    if args.points_shape == "partial":
+        is_partial = True
+    elif args.points_shape == "whole":
+        is_partial = False
+    else:
+        NotImplementedError
+
+
     root_list = [root_1, root_2]
     dataset_list = []
     for root in root_list:
-        dataset_list.append(UOPSim(root,
+        dataset_list.append(UOPSIM(root,
                             sampling='random',
-                            partial=False,  #TODO
-                            projection=False))
+                            partial=is_partial,  
+                            ))
     dataset = MergeDataset(dataset_list)
     data_length  = dataset.dataset_size
     tr_data, val_data = random_split(dataset, [round(data_length * 0.8), round(data_length * 0.2)], generator=torch.Generator().manual_seed(777))
